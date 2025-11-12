fd1bc06d2b8bbf8a5c61bbf93fc04fe76ba1c736
Seung woo Kim <ipod6656@gmail.com>
2025-11-12 14:02:31 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7490725313-b0a24",
+  "appId": "1:623632579535:web:6f102cebe5a4f3830cf0ba",
+  "apiKey": "AIzaSyA_7fiVQ4ohHgFRGOYCJlP7IvMlO5PV1jI",
+  "authDomain": "studio-7490725313-b0a24.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "623632579535"
+};
